fb677213d3bcf9b5ca4b90425ea1879be345f3bf
makender103 <makender103@gmail.com>
2020-10-11 10:19:28 -0300

    implementation of the next button

---
+++ script.js
@@ -0,0 +1,23 @@
+const slides = document.querySelectorAll('[data-js = "carousel__item"]')
+const nextButton = document.querySelector('[data-js="carousel__button--next"]')
+
+let currentSlideIndex = 0
+
+nextButton.addEventListener('click', () => {
+  if (currentSlideIndex === slides.length-1) {
+    //
+    currentSlideIndex = 0
+  } else {
+    //
+    currentSlideIndex++
+  }
+  
+  //for removing the next slide when the Next button was pressed
+  slides.forEach(slide => {
+    slide.classList.remove('carousel__item--visible')
+  })
+
+  slides[currentSlideIndex].classList.add('carousel__item--visible')
+})
+
+
