c3664d3a0882b2104e35288398c12a94a95198da
makender103 <makender103@gmail.com>
2020-10-11 10:47:29 -0300

    the next button

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const slides = document.querySelectorAll('[data-js = "carousel__item"]')
 const nextButton = document.querySelector('[data-js="carousel__button--next"]')
+const prevButton = document.querySelector('[data-js="carousel__button--prev"]')
 
 let currentSlideIndex = 0
 
 nextButton.addEventListener('click', () => {
-  if (currentSlideIndex === slides.length-1) {
+  if (currentSlideIndex === slides.length -1) {
     //
     currentSlideIndex = 0
   } else {
-    //
     currentSlideIndex++
   }
   
@@ -21,3 +21,21 @@ nextButton.addEventListener('click', () => {
 })
 
 
+prevButton.addEventListener('click',()=> {
+  // if (currentSlideIndex === 0) {
+  //   currentSlideIndex = slides.length -1
+  // } else {
+  //   currentSlideIndex--
+  // }
+
+  
+  const correctSlideIndex =  currentSlideIndex === 0 
+  ? currentSlideIndex = slides.length -1 
+  : --currentSlideIndex
+
+  slides.forEach(slide => {
+    slide.classList.remove('carousel__item--visible')
+  })
+
+  slides[correctSlideIndex].classList.add('carousel__item--visible')
+})
